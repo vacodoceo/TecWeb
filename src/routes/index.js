97dec345eef3d8c215feb159ff5553fc5ddfbd10
Verner Codoceo <vacodoceo@uc.cl>
2018-10-11 02:48:59 -0300

    Changed index links to Log In and Register

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,7 @@ const router = new KoaRouter();
 router.get('/', async (ctx) => {
   await ctx.render('index', { 
     appVersion: pkg.version,
-    PublicationsPath: ctx.router.url('publications'),
-    UsersPath: ctx.router.url('users') 
+    newUserPath: ctx.router.url('users-new'), 
   });
 });
 
